41ebe339d101ab2515d0060ff96651aa8412b1bf
Allen Lin <allen@deansoft.com.tw>
2015-04-12 14:55:54 +0800

    Add review, rating, specifics and signup

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 import core.models as coremodels
 
-# Register your models here.
+
 admin.site.register(coremodels.Location)
+admin.site.register(coremodels.Review)
